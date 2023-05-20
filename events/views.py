3122d75a2b2e894f6440d8e54f597d47f923c84f
ceimos <86879499+ceimos@users.noreply.github.com>
2023-05-21 01:04:39 +0530

    fixes : csrf_exempt, bugs_create_event

--- events/views.py
+++ events/views.py
@@ -7,26 +7,34 @@ from django.core import serializers
 from django.shortcuts import render, get_object_or_404
 from django.views import View
 
+#from django.views.decorators.csrf import csrf_exempt
 
+#@csrf_exempt
 def event_index(request):
     event_list=Event.objects.all()
     json_data=serializers.serialize('json',event_list)
     return JsonResponse(json_data, safe=False)
-    
+
+#@csrf_exempt  
 def create_event(request):
     if request.method == 'POST':
-        name = request.POST.get('name')
+        name = request.POST.get('name') #current error - NOT NULL constraint failed: events_event.name
         time = request.POST.get('time')
         location = request.POST.get('location')
         capacity = request.POST.get('capacity')
-        tag = request.POST.get('tag')
+        tags = request.POST.getlist('tag')
+        host_id = request.POST.get('host') #receive the id of the host
+        host = User.objects.get(id=host_id) # 
+        description = request.POST.get('description')
   
         event = Event.objects.create(name=name, time=time, location=location,capacity=capacity,
-                                    tag=tag)
+                                     host=host, description=description)
+        event.tag.set(tags)
         return JsonResponse({'event_id': event.id})
     else:
             return JsonResponse({'error': 'Invalid request method'})
 
+#@csrf_exempt
 def event_detail(request, event_id):
     event=get_object_or_404(Event, id=event_id)
     tags= list(event.tag.values())
@@ -42,6 +50,7 @@ def event_detail(request, event_id):
     }
     return JsonResponse(data)
 
+#@csrf_exempt
 def register_user(request):
     if request.method == 'POST':
         name = request.POST.get('name')
@@ -52,12 +61,15 @@ def register_user(request):
     else:
             return JsonResponse({'error': 'Invalid request method'})
 
+#@csrf_exempt
 def create_tag(request):
     if request.method == 'POST':
         name = request.POST.get('name')
   
-        user = Tag.objects.create(name=name)
-        return JsonResponse({'event_id': user.id})
+        tag = Tag.objects.create(name=name)
+        return JsonResponse({'event_id': tag.id})
     else:
             return JsonResponse({'error': 'Invalid request method'})
     
+#Add_attendee() --> called when invite accepted, or going to an event.
+#make_post(,event.id) --> replies(m), 
