b4ea641fd310d59eb7b7ec19653447344adc201c
Scott Ankiewicz <scottfa.a@gmail.com>
2023-05-20 12:04:03 -0400

    fix: remove default values on foreign keys

--- events/models.py
+++ events/models.py
@@ -1,25 +1,30 @@
 from django.db import models
 
+
 class User(models.Model):
     name = models.CharField(max_length=500)
     email = models.CharField(max_length=1000)
 
+
 class Tag(models.Model):
-    name=models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
 
     def __str__(self):
         return self
+
 
 class Event(models.Model):
     name = models.CharField(max_length=100)
     time = models.DateTimeField()
     location = models.CharField(max_length=200)
-    host = models.ForeignKey(User,on_delete=models.CASCADE, default=22)
+    host = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     description = models.CharField(max_length=10000)
     capacity = models.IntegerField()
-    attendees = models.ManyToManyField(User, related_name='events_attending') #reverse relationship
+    attendees = models.ManyToManyField(
+        User, related_name="events_attending"
+    )  # reverse relationship
     tag = models.ManyToManyField(Tag)
-    
+
     def __str__(self):
         return self.name[0:30]
 
@@ -34,5 +39,3 @@ class Post(models.Model):
 
     def __str__(self):
         return self.text[0:30]
-
-    
